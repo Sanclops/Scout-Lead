56558ddad96ba26b9094f6d4e2e1b16aef58a759
Sankalp Agrawal <sanclops@gmail.com>
2016-03-12 20:56:48 -0800

    Initial Commit

---
+++ Scout Lead/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Scout Lead
+//
+//  Created by Sankalp Agrawal on 3/12/16.
+//  Copyright © 2016 Sankalp Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Scout Lead/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Scout Lead
+//
+//  Created by Sankalp Agrawal on 3/12/16.
+//  Copyright © 2016 Sankalp Agrawal. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
